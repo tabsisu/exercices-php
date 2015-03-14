c86e60851b91f157f60c9a58093b04b208fb522c
tabsisu <tabeasuter1@gmail.com>
2015-03-14 11:11:25 +0100

    Create edit.php
    Voici mon nouveau edit.php

---
+++ te/ch04/te1/edit.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+/* ---------------------------------------------------------------------
+* Test 1
+* Nom du fichier: cities.php
+* Auteur: Tabea Suter
+* Date: Mars 2015
+* Description: 
+* ---------------------------------------------------------------------
+*/
+// Constantes
+//Pour se connecter à la base de données
+const DB_SERVER = 'localhost';
+const DB_USER = 'cpnv';
+const DB_PWD = 'cpnv1234';
+const DB_NAME = 'world';
+
+?>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>te1</title>
+  </head>
+  <body>
+ <p> Vous pouvez maintenant modifier les données.</p>
+ <a href="cities.php">Revenir à la page précédente</a> 
+  </body>
+</html>
